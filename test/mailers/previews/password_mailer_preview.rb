bfb45ae847ab3e725fa15981674c3c2899ccdafc
iuhoay <iuhoay@gmail.com>
2024-06-24 19:36:20 +0800

    Preview password reset email

--- test/mailers/previews/password_mailer_preview.rb
+++ test/mailers/previews/password_mailer_preview.rb
@@ -1,4 +1,8 @@
 # Preview all emails at http://localhost:3000/rails/mailers/password_mailer
 class PasswordMailerPreview < ActionMailer::Preview
-
+  def password_reset
+    user = User.new(email: "preview@example.org")
+    token = user.generate_token_for(:password_reset)
+    PasswordMailer.with(user: user.email, token: token).password_reset
+  end
 end
